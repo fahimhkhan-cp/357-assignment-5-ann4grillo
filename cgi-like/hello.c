0231bf490d6a0b05c42384edeb2d35fe5644d519
Anna Rose Grillo <argrillo@unix3.csc.calpoly.edu>
2024-11-21 09:10:01 -0800

    added

---
+++ cgi-like/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(){
+    printf("hello!\n");
+            char *program = "test";
+                char newpath[30] = "/cgi-like/";
+            strcat(newpath, program);
+            printf("newpath: %s\n", newpath);
+}
